d18394158cc9eae06ab10c55c21cce003efed88d
Luis Cruz <luiscruzdesign@gmail.com>
2020-09-21 00:59:41 -0300

    Chat

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,7 +1,9 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonFooter } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import TituloPagina from '../components/TituloPagina';
+import ItemClienteProposta from '../components/ItemClienteProposta';
 import Topo from '../components/Topo';
+import './styles/Chat.css';
 
 const Chat: React.FC = () => {
   return (
@@ -10,13 +12,44 @@ const Chat: React.FC = () => {
         <Topo />
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Chat</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <TituloPagina />
+        <ItemClienteProposta />
+        <div className="default-side-padding" style={{paddingTop: '10px'}}>
+          <div className="chat-message left">
+            <p>
+              Olá, tudo bem? 
+              <br></br>
+              <br></br>
+              Gostaria de fazer a locação da sua cozinha, é uma ótima localização para o meu negócio, e sua estrutura é perfeita para o que preciso. 
+              <br></br>
+              <br></br>
+              Trabalho há 5 anos no mercado e possuo unidades no Morumbi e em Itaquera. Você pode conferir minha empresa em www.cookiesandcream.com.br 
+              <br></br>
+              <br></br>
+              Obrigado.
+            </p>
+            <p>10/09/2020 às 19:52</p>
+          </div>
+          <div className="chat-message right">
+            <p>
+              Tudo ótimo e com você? 
+              <br></br>
+              <br></br>
+              Podemos fechar um ótima parceria. Irei aceitar a proposta, ligue no número (11) 98765-4372, falar com Paulo. 
+            </p>
+            <p>11/09/2020 às 09:20</p>
+          </div>
+          <p className="chat-system success">Proposta aceita em 11/09/2020 às 09:21</p>
+        </div>
       </IonContent>
+      <IonFooter className="white-footer">
+        <div className="input-text-chat">
+          <input type="text" placeholder="Mensagem"/>
+          <div className="chat-message-submit">
+            <span></span>
+          </div>
+        </div>
+      </IonFooter>
     </IonPage>
   );
 };
