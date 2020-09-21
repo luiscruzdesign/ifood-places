92c7b7de643d71f70152454bd72cf436d370a393
Luis Cruz <luiscruzdesign@gmail.com>
2020-09-21 03:18:04 -0300

    Cliente enviar proposta

---
+++ src/components/ResumoLocalPropostaCliente.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Endereco from '../components/Endereco';
+import './styles/ResumoLocalPropostaCliente.css';
+
+interface ContainerProps { }
+
+const ResumoLocalPropostaCliente: React.FC<ContainerProps> = () => {
+  return (
+    <div className="resumo-local">
+      <h1>Jaf Burgers</h1>
+      <Endereco />
+      <div>
+        <p><strong>Estrutura</strong></p>
+        <p>Bocas de fogão <strong>8</strong></p>
+        <p>Fornos <strong>2</strong></p>
+        <p>Bancadas <strong>5</strong></p>
+        <p>Fritadeiras <strong>2</strong></p>
+      </div>
+      <div>
+        <p><strong>Segunda a sábado</strong></p>
+        <p>0h às 18h</p>
+      </div>
+      <div>
+        <p><strong>Domingo</strong></p>
+        <p>0h às 0h</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResumoLocalPropostaCliente;
--- src/pages/clienteLocalEnviarProposta.tsx
+++ src/pages/clienteLocalEnviarProposta.tsx
@@ -1,22 +1,71 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonSelect, IonSelectOption, IonFooter, IonButton } from '@ionic/react';
+import React, { useState } from 'react';
+import TituloPagina from '../components/TituloPagina';
 import Topo from '../components/Topo';
+import ResumoLocalPropostaCliente from '../components/ResumoLocalPropostaCliente';
+import './styles/ClienteLocalEnviarProposta.css';
 
 const ClienteLocalEnviarProposta: React.FC = () => {
+  const [periodo, setPeriodo] = useState<string>();
+
   return (
     <IonPage>
       <IonHeader>
         <Topo />
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Cliente  local - enviar proposta</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <TituloPagina />
+        <ResumoLocalPropostaCliente />
+        <div className="default-side-padding" style={{paddingTop: '10px'}}>
+          <h2 className="section-title">Horários para locação</h2>
+          <IonSelect value={periodo} placeholder="Selecione um período" onIonChange={e => setPeriodo(e.detail.value)}>
+            <IonSelectOption value="dias-de-semana">Dias de semana</IonSelectOption>
+            <IonSelectOption value="final-de-semana">Final de semana</IonSelectOption>
+            <IonSelectOption value="sex-sab-dom">Sexta-feira, sábado e domingo</IonSelectOption>
+            <IonSelectOption value="seg">Segunda-feira</IonSelectOption>
+            <IonSelectOption value="ter">Terça-feira</IonSelectOption>
+            <IonSelectOption value="que">Quarta-feira</IonSelectOption>
+            <IonSelectOption value="qui">Quinta-feira</IonSelectOption>
+            <IonSelectOption value="sex">Sexta-feira</IonSelectOption>
+            <IonSelectOption value="sab">Sábado</IonSelectOption>
+            <IonSelectOption value="dom">Domingo</IonSelectOption>
+          </IonSelect>
+          <div className="simple-divider"></div>
+          <div className="horario-controller">
+            <div>
+              <div className="horario-input">
+                <p>Início:</p>
+                <input type="number" max="24" />
+                <span>:</span>
+                <input type="number" max="60" />
+              </div>
+              <div className="simple-divider"></div>
+              <div className="horario-input">
+                <p style={{marginRight: '30px'}}>Fim:</p>
+                <input type="number" max="24" />
+                <span>:</span>
+                <input type="number" max="60" />
+              </div>
+            </div>
+            <div className="circle-button success">
+              <img src="assets/img/check.svg" alt="Confirmar"/>
+            </div>
+          </div>
+          <div className="simple-divider"></div>
+          <div className="simple-divider"></div>
+          <div className="simple-divider"></div>
+          <h2 className="section-title">Valor hora</h2>
+          <input type="number" placeholder="Insira o valor em reais" className="input-valor-hora" />
+          <div className="simple-divider"></div>
+          <div className="simple-divider"></div>
+          <div className="simple-divider"></div>
+          <h2 className="section-title">Mensagem</h2>
+          <textarea placeholder="Mensagem" />
+        </div>
       </IonContent>
+      <IonFooter className="white-footer">
+        <IonButton expand="full" color="success">Enviar proposta</IonButton>
+      </IonFooter>
     </IonPage>
   );
 };
